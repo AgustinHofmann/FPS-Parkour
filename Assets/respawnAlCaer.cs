53ba7c33943944d254d96cfa3593fbdecf0c47fe
Agustin Hofmann <agustinhofmann@gmail.com>
2025-05-19 08:56:54 -0300

    ParkourYRespawn

---
+++ Assets/respawnAlCaer.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class respawnAlCaer : MonoBehaviour
+{
+
+    public float yRespawnValue;
+
+    void Start()
+    {
+    
+        
+
+    }
+
+    
+    void Update()
+    {
+
+        if (transform.position.y < yRespawnValue)
+        {
+            //Debug.Log("Me cai");
+            SceneManager.LoadScene("FPS Parkour");
+        }
+
+    }
+}
